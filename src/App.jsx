c0d40c2da7a601e4d9203eede0281d81ceff5537
Mai V. Xuan Vuong <xuannvuongg@gmail.com>
2024-04-03 21:17:16 +0700

    "first commit"

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CommingSoon from "./page/commingsoon/CommingSoon"
+
+function App() {
+
+  return (
+    <>
+    <CommingSoon />
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        // Background & Text
+        // 'bg': '#2B2B2B',
+        // 'bgsecondary': '#3B3B3B',
+        // 'captiontext': '#858584',
+        // 'textW': '#FFF',
+        // // Call to Action
+        // 'callAcction': '#A259FF',
+      },
+      backgroundImage: {
+        // Gradients
+        // 'gradient1': 'linear-gradient(101deg, #A259FF 13.57%, #FF6250 97.65%)',
+        // 'gradient2': 'linear-gradient(128deg, #A259FF 49.75%, #377DF7 136.56%)',
+      },
+    },
+  },
+  plugins: [],
+};
